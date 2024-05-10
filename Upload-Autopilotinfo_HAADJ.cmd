ec53290a7edf42660f782ec432aef9223b39460f
justinkron <justinkron@gmail.com>
2024-05-10 14:07:17 -0500

    Add files via upload

---
+++ Upload-Autopilotinfo_HAADJ.cmd
@@ -0,0 +1,10 @@
+rem version 1.5 - For Hybrid Azure AD Joined Devices. 
+rem version 1.6 - added diagnostics script.
+@echo on
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}" -ExecutionPolicy Bypass
+powershell -Command "& {Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force}" -ExecutionPolicy Bypass
+powershell -Command "& {Install-Script -name Get-WindowsAutopilotInfo -Force}"  -ExecutionPolicy Bypass
+powershell -Command "& {Install-Script -name Get-AutopilotDiagnostics -Force}"  -ExecutionPolicy Bypass
+powershell -Command "& {set-executionpolicy RemoteSigned}"
+
+powershell.exe -file "C:\Program Files\WindowsPowerShell\Scripts\Get-WindowsAutoPilotInfo.ps1" -Online -Assign -reboot
