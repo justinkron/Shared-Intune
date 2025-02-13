4b6910dc0b123be2482db046f5fbcf993aea7cd9
justinkron <justinkron@gmail.com>
2025-02-13 14:17:52 -0600

    Update Upload-Autopilotinfo_HAADJ.cmd
    fix for line 11

--- Upload-Autopilotinfo_HAADJ.cmd
+++ Upload-Autopilotinfo_HAADJ.cmd
@@ -8,7 +8,7 @@ powershell -Command "& {Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 
 powershell -Command "& {Install-Script -name Get-WindowsAutopilotInfo -Force}"  -ExecutionPolicy Bypass
 powershell -Command "& {Install-Script -name Get-AutopilotDiagnostics -Force}"  -ExecutionPolicy Bypass
 powershell -Command "& {Install-Script -name Autopilottestattestation -Force}"  -ExecutionPolicy Bypass
-powershell -Command "& {reg add "HKLM\SOFTWARE\Microsoft\Internet Explorer\Main" /v "DisableFirstRunCustomize" /t REG_DWORD /d 1 /f}" -ExecutionPolicy Bypass
+powershell -Command "& {reg add 'HKLM\SOFTWARE\Microsoft\Internet Explorer\Main' /v 'DisableFirstRunCustomize' /t REG_DWORD /d 1 /f}" -ExecutionPolicy Bypass
 powershell -Command "& {set-executionpolicy RemoteSigned}"
 
 powershell.exe -file "C:\Program Files\WindowsPowerShell\Scripts\Get-WindowsAutoPilotInfo.ps1" -Online -Assign -reboot
